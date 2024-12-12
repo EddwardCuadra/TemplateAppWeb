738dc1c03d3d37eb52f322025192f530c7f7a08a
EddwardCuadra <41312733+EddwardCuadra@users.noreply.github.com>
2024-12-12 19:11:32 +0000

    se añaden rutas de static

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,6 @@
 # app/routes.py
-from flask import Blueprint, jsonify, render_template
-
+from flask import Blueprint, jsonify, render_template, send_from_directory, request, redirect, url_for
+import os
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
@@ -10,3 +10,16 @@ def home():
 @main_bp.route('/soporte')
 def soporte():
     return jsonify({'message': 'Página de soporte'})
+
+#Rutas de Statics Css Js Img
+@main_bp.route('/css/<path:filename>')
+def css(filename):
+    return send_from_directory(os.path.join('static', 'css'), filename)
+
+@main_bp.route('/js/<path:filename>')
+def js(filename):
+    return send_from_directory(os.path.join('static', 'js'), filename)
+
+@main_bp.route('/img/<path:filename>')
+def img(filename):
+    return send_from_directory(os.path.join('static', 'img'), filename)
