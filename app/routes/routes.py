430195d95c263e28f80087656769feec9b074e5e
EddwardCuadra <41312733+EddwardCuadra@users.noreply.github.com>
2024-12-13 10:06:03 +0000

    se añadio la plantilla de estilos bootstrap

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -11,15 +11,23 @@ def home():
 def soporte():
     return jsonify({'message': 'Página de soporte'})
 
-#Rutas de Statics Css Js Img
-@main_bp.route('/css/<path:filename>')
+#Rutas de Statics Scss Css Js Img Vendor
+@main_bp.route('/assets/css/<path:filename>')
 def css(filename):
     return send_from_directory(os.path.join('static', 'css'), filename)
 
-@main_bp.route('/js/<path:filename>')
+@main_bp.route('/assets/js/<path:filename>')
 def js(filename):
     return send_from_directory(os.path.join('static', 'js'), filename)
 
-@main_bp.route('/img/<path:filename>')
+@main_bp.route('/assets/img/<path:filename>')
 def img(filename):
     return send_from_directory(os.path.join('static', 'img'), filename)
+
+@main_bp.route('/assets/scss/<path:filename>')
+def scss(filename):
+    return send_from_directory(os.path.join('static', 'scss'), filename)
+
+@main_bp.route('/assets/vendor/<path:filename>')
+def vendor(filename):
+    return send_from_directory(os.path.join('static', 'vendor'), filename)
